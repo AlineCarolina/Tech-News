a0b0c7cf099f238dd791a73dd816cf7756f44126
Aline <eualine2017@gmail.com>
2022-07-01 19:30:37 -0300

    requisito 1

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -1,6 +1,20 @@
+import requests
+import time
+
+
 # Requisito 1
 def fetch(url):
-    """Seu código deve vir aqui"""
+    time.sleep(1)
+    try:
+        response = requests.get(
+            url, headers={"user-agent": "Fake user-agent"}, timeout=3
+        )
+        if response.status_code != 200:
+            return None
+        else:
+            return response.text
+    except response.Timeout:
+        return None
 
 
 # Requisito 2
