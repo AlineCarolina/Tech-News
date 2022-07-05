114e986099c66decdc760414f265c46c78219383
Aline <eualine2017@gmail.com>
2022-07-05 19:44:51 -0300

    requisito 11

--- tech_news/analyzer/ratings.py
+++ tech_news/analyzer/ratings.py
@@ -1,5 +1,5 @@
 # Requisito 10
-from tech_news.database import db
+from tech_news.database import db, get_collection
 
 
 def top_5_news():
@@ -12,4 +12,11 @@ def top_5_news():
 
 # Requisito 11
 def top_5_categories():
-    """Seu código deve vir aqui"""
+    top_noticias = get_collection().aggregate(
+        [
+            {"$group": {"_id": "$category", "count": {"$sum": 1}}},
+            {"$sort": {"count": -1}},
+            {"$limit": 5},
+        ]
+    )
+    return [item["_id"] for item in top_noticias]
